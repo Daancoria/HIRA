84de73d51eabedbe9742dd40e8f054f917f7b67f
ElMightyVato <jairgranadosramirez@gmail.com>
2025-06-14 11:00:42 -0500

    Connected to database need team to check for approval!

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,2 +1,23 @@
+# app/__init__.py
+
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from app.config import Config
+
+# Import models after db object created
 from app.models import db
-db.init_app()
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
+
+    # Initialize db
+    db.init_app(app)
+
+    # Test route to verify connection
+    @app.route("/")
+    def index():
+        return "HIRA Backend Connected!"
+
+    return app
+
